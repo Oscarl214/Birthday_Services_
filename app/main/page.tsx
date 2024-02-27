b7082c5572ef790c3358f7d3119e82897cb0d4cc
Oscar Leal <oscarleal234@gmail.com>
2024-02-27 12:59:49 -0600

    added hero to main page

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import Image from 'next/image';
+import MainHero from '../components/Main/hero';
 const Main = () => {
-  return <div></div>;
+  return (
+    <div className="bg-black">
+      <MainHero />
+    </div>
+  );
 };
 
 export default Main;
