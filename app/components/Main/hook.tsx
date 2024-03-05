dbf122854ce42045b6dab3ecfceb01a26d7a340e
Oscar Leal <oscarleal234@gmail.com>
2024-03-05 16:47:55 -0600

    added contactus section to main page

--- app/components/Main/hook.tsx
+++ app/components/Main/hook.tsx
@@ -26,8 +26,8 @@ const Hook = () => {
         </li>
       </ul>
       <div className="flex items-center justify-center mt-9">
-        <button className="border-black border-2 border-solid  p-3 lg:text-md text-sm font-semibold text-black hover:bg-red-400 rounded-full hook-button">
-          HIRE US
+        <button className="border-red-700 border-2 border-solid  p-3 lg:text-md text-sm font-semibold text-black hover:bg-red-400 rounded-full hook-button">
+          ABOUT US
         </button>
       </div>
     </div>
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Image from 'next/image';
 import MainHero from '../components/Main/hero';
 import Hook from '../components/Main/hook';
+import AboutUs from '../components/Main/aboutus';
 
 const Main = () => {
   return (
     <div className=" pt-9">
       <MainHero />
       <Hook />
+      <AboutUs />
     </div>
   );
 };
