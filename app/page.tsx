e734c13e687ff371bb97480829cc25d1b0e0fbc0
Oscar Leal <oscarleal234@gmail.com>
2024-03-13 20:39:42 -0500

    testing3

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import Main from './main/page';
-import { usePathname } from 'next/navigation';
+import MainHero from './components/main/hero';
+import Hook from './components/main/hook';
+import TitleImage from './components/main/image';
+import AboutUsMain from './components/main/aboutusmain';
 
 export default function Home() {
   return (
     <main>
-      <Main />
+      <MainHero />
+      <Hook />
+      <TitleImage />
+      <div className="bg-black">
+        <AboutUsMain />
+      </div>
     </main>
   );
 }
