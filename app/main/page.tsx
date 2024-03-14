035a17936289bbd8faff9644c10be467444e7005
Oscar Leal <oscarleal234@gmail.com>
2024-03-13 20:17:34 -0500

    added fixes

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Image from 'next/image';
 import MainHero from '../components/main/hero';
 import Hook from '../components/main/hook';
 import AboutUsHero from '../components/main/contactus';
 import TitleImage from '../components/main/image';
 import AboutUsMain from '../components/main/aboutusmain';
-import { MotionDiv } from '../components/main/motion';
 
 const Main = () => {
   return (
